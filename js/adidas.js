5af7bbfef22e1ca9340156d53cc3e093a972d4b2
eileentwo <578194597@qq.com>
2017-08-16 17:34:36 +0800

    增加JQ部分

---
+++ js/adidas.js
@@ -0,0 +1,194 @@
+$(function(){
+		var $oUl =$('#playimages').find('ul');
+		var $aLi = $('#playimages').find('li');
+		var $imgCon = $('#playimages').find('.con-fangDaIMg'),//正常图片容器
+		    $Drag = $('#playimages').find('.magnifyingBegin'),//拖动滑动容器
+		    $show = $('#playimages').find('.magnifyingShow'),//放大镜显示区域
+		    $showIMg = $show.find('img');//放大镜图片
+		var $oImg = $('#playimages').find('.img');
+		
+		var $aimg = $oUl.find('img');
+		var $src = $aimg.attr('src');
+		var $prev = $('#playimages').find('.prev');
+		var $next = $('#playimages').find('.next');
+		var $len = $aLi.length;
+		var num=0;
+		var $timer = null;
+		var liW = $aLi.eq(0).outerWidth();
+		var W =liW*$len;
+		
+		
+		$oUl.css('width',W); 
+		$oImg.attr('src',$src);
+		$aimg.eq(0).attr('class','active');
+		
+		$aLi.mouseover(function(){
+			var i =$(this).index();
+			var src = $aimg.eq(i).attr('src');
+			console.log(src)
+			$oImg.attr('src',src);
+			$aimg.attr('class','');
+			$showIMg.attr('src',src);
+			
+			$aimg.eq(i).attr('class','active');
+			
+			num =i;
+		});
+		
+		$next.mouseover(function(){
+			$(this).addClass('active');
+		})
+		$next.mouseout(function(){
+			$(this).removeClass('active');
+		})
+		$prev.mouseover(function(){
+			$(this).addClass('active');
+		})
+		$prev.mouseout(function(){
+			$(this).removeClass('active');
+		})
+		
+		$next.click(function(){
+			num++;
+			if(num>$len-1){//顺序式轮播
+				num=$len-1
+			}
+//					num=num%5;//循环式的轮播
+			$aimg.attr('class','');
+			$aimg.eq(num).attr('class','active');
+			var src = $aimg.eq(num).attr('src');
+			$oImg.attr('src',src);
+			$showIMg.attr('src',src);
+			$("#wrap").ulScroll();
+		})
+		$prev.click(function(){
+			num--;
+//					num=(num+5)%5;//循环式的轮播
+			if(num==-1){
+				num=0
+			}
+			$aimg.attr('class','');
+			$aimg.eq(num).attr('class','active');
+			var src = $aimg.eq(num).attr('src');
+			$oImg.attr('src',src);
+			$showIMg.attr('src',src);
+			$("#wrap").ulScroll();
+		})
+		$.fn.ulScroll = function(){
+			if(num<$len- 2){
+				$oUl.animate({'marginLeft':(-liW+3 )*(num-2?0:(num-2)) });
+			}
+			if(num>=$len- 2){
+				$oUl.animate({'marginLeft':(-liW+3 )*($len-4) });
+			}
+			
+		}
+		
+		
+	$.fn.magnifying = function(){
+		var multiple = $show.width()/$Drag.width();
+		
+		$imgCon.mousemove(function(e){
+			$Drag.css('display','block');
+			$show.css('display','block');
+		    //获取坐标
+		   	var iX = e.pageX - $(this).offset().left - $Drag.width()/2,
+		   		iY = e.pageY - $(this).offset().top - $Drag.height()/2,	
+		   		MaxX = $imgCon.width()-$Drag.width(),
+		   		MaxY = $imgCon.height()-$Drag.height();
+
+		   	iX = iX > 0 ? iX : 0;
+		   	iX = iX < MaxX ? iX : MaxX;
+		   	iY = iY > 0 ? iY : 0;
+		   	iY = iY < MaxY ? iY : MaxY;	
+		   	$Drag.css({left:iX+'px',top:iY+'px'});	   		
+		   	$showIMg.css({marginLeft:-multiple*iX+'px',marginTop:-multiple*iY+'px'});
+		   	
+		});
+		$imgCon.mouseout(function(){
+		   	$Drag.css('display','none');
+			$show.css('display','none');
+		});
+	}
+	$("#playimages").magnifying();
+	
+	
+	//尺码部分
+	$sSpan = $('#size').find('span');
+	$sSpan.on('click mouseover',function(){
+		$sSpan.css('border-color','#ccc');
+		$sSpan.eq($(this).index()).css('border-color','red');
+	})
+	//吸顶部分
+	var b = $('#detailsNav').offset().top;
+	$(document).on('scroll',function(){
+		var c = $(document).scrollTop();
+		if(b<=c){
+			$('#detailsNav').css('position','fixed');
+			$('#detailsNav').css('left','415px');
+			$('#detailsNav').css('background-color','white');
+			$('#detailsNav a').eq(3).css('display','block');
+		}else{
+			$('#detailsNav').css('position','absolute');
+			$('#detailsNav').css('left','0');
+			$('#detailsNav').css('background-color','#F8F8F8');
+			$('#detailsNav a').eq(3).css('display','none');
+		}
+	})
+	$DNa = $('#detailsNav').find('.DRnav');
+	$DNa.click(function(){
+		$(this).addClass('active').siblings().removeClass('active');
+	})
+	
+	//数量加减
+	var t = $("#text_box");
+	//初始化数量为1,并失效减
+	$('#min').attr('disabled',true);
+	 //数量增加操作
+	 $("#add").click(function(){ 
+	  // 给获取的val加上绝对值，避免出现负数
+		t.val(Math.abs(parseInt(t.val()))+1);
+		if (parseInt(t.val())!=1){
+			$('#min').attr('disabled',false);
+		};
+	 }) 
+	 //数量减少操作
+	 $("#min").click(function(){
+		 t.val(Math.abs(parseInt(t.val()))-1);
+		 if (parseInt(t.val())==1){
+		 	$('#min').attr('disabled',true);
+		 };
+	 })
+	$("#add").mouseover(function(){
+		$(this).css('background-color','#eee');
+		$("#min").css('background-color','#fff');
+	})
+	$("#min").mouseover(function(){
+		$(this).css('background-color','#eee');
+	})
+	$("#add").mouseout(function(){
+		$(this).css('background-color','#fff');
+	})
+	$("#min").mouseout(function(){
+		$(this).css('background-color','#fff');
+	})
+	$("#min").css('background-color','#eee');
+	
+	
+	//右边导航
+	var d = $('#right-aside').offset().top-50;
+	$(document).on('scroll',function(){
+		var c = $(document).scrollTop();
+		if(d<=c){
+			$('#right-aside').css('position','fixed');
+			$('#right-aside').css('top','50px');
+			$('#right-aside').css('left','87%');
+		}else{
+			$('#right-aside').css('position','absolute');
+			$('#right-aside').css('top','66px');
+			$('#right-aside').css('left','101%');
+		}
+	})
+	
+	
+})
---
+++ js/kaola.js
@@ -0,0 +1,183 @@
+$(function(){
+    //第一张显示
+    $(".bankImg li").eq(0).show();
+    
+    $("#list li").mouseover(function() {
+        $(this).addClass('cur').siblings().removeClass("cur");
+        var index = $(this).index();
+        i = index;//不加这句有个bug，鼠标移出小圆点后，自动轮播不是小圆点的后一个
+        $(".bankImg li").eq(index).fadeIn(500).siblings().fadeOut(500);
+    });
+    
+    var i=0;
+    var timer=setInterval(play,2000);
+    
+    var play=function(){
+        i++;
+        i = i > $(".bankImg li").length-1 ? 0 : i ;
+        
+        $("#list li").eq(i).addClass('cur').siblings().removeClass("cur");
+        $(".bankImg li").eq(i).fadeIn(500).siblings().fadeOut(500);
+    }
+    
+    var playLeft=function(){
+        i--;
+        i = i < 0 ? $(".bankImg li").length-1 : i ;
+        $("#list li").eq(i).addClass('cur').siblings().removeClass("cur");
+        $(".bankImg li").eq(i).fadeIn(500).siblings().fadeOut(500);
+    }
+    
+    $("#banks-inner").hover(function() {
+        clearInterval(timer);
+    }, function() {
+        timer=setInterval(play,2000);
+    });
+    
+    $("#prev").click(function(){
+        playLeft();
+    })
+    $("#next").click(function(){
+        play();
+    })
+    $("#prev").mouseover(function(){
+    	$(this).css('background-position','-626px -153px')
+    })
+    $("#next").mouseover(function(){
+    	$(this).css('background-position','-670px -154px')
+    })
+    $("#prev").mouseout(function(){
+    	$(this).css('background-position','-626px -68px')
+    })
+    $("#next").mouseout(function(){
+    	$(this).css('background-position','-670px -68px')
+    })
+    $("#banks-inner").mouseover(function(){
+    	$("#next").show();
+    	$("#prev").show();
+    })
+    $("#banks-inner").mouseout(function(){
+    	$("#next").hide();
+    	$("#prev").hide();
+    })
+    
+    
+    
+    //两边导航
+	var d = $('#right-aside').offset().top-50;
+	$(document).on('scroll',function(){
+		var c = $(document).scrollTop();
+		if(d<=c){
+			$('#right-aside').css('position','fixed');
+			$('#content-aside').css('position','fixed');
+			$('#right-aside').css('top','50px');
+			$('#right-aside').css('left','87%');
+			$('#content-aside').css('top','50px');
+			$('#content-aside').css('left','6%');
+		}else{
+			$('#right-aside').css('position','absolute');
+			$('#content-aside').css('position','absolute');
+			$('#right-aside').css('top','66px');
+			$('#right-aside').css('left','101%');
+			$('#content-aside').css('top','50px');
+			$('#content-aside').css('left','-12%');
+		}
+	})
+	//返回顶部
+	$(function(){
+	  $("#backTop").click(function() {
+	      $("html,body").animate({scrollTop:0}, 1000);
+	  }); 
+	 })
+})
+
+
+//window.onload = function (){
+//	var Ltitle = document.getElementById('funcTab-left-title');
+//	var allUl =  document.getElementById('funcTab-left-ul');
+//	
+//	function Tover(){
+//		allUl.style.display = 'block';
+//	}
+//	function Tout(){
+//		allUl.style.display = 'none';
+//	}
+//	allUl.onmouseover = Ltitle.onmouseover =Tover;
+//	allUl.onmouseout = Ltitle.onmouseout =Tout;
+//	//轮播图部分
+//	var oDiv = document.getElementById('banks-inner');
+//	var oPrev = oDiv.getElementsByTagName('a')[0];
+//	var oNext = oDiv.getElementsByTagName('a')[1];
+//	var oImg = oDiv.getElementsByTagName('img')[0];
+//	var oUl = oDiv.getElementsByTagName('ul')[0];
+//	var aLi = oUl.getElementsByTagName('li');
+//	
+//	var arrUrl =['img/index-1/1bgo7pq2249_1920_402.jpg','img/index-1/1bgo42ot26_1920_401.jpg','img/index-1/1bgo43tfg19_1920_403.jpg',
+//	'img/index-1/1bgo3h3qe28_1920_404.jpg','img/index-1/1bftp22gp37_1920_405.jpg','img/index-1/1bftp22gp37_1920_406.jpg'];
+//
+//	var num =0;
+//	
+//	for ( var i=0; i<arrUrl.length; i++){
+//		oUl.innerHTML +='<li>' + i + '</li>';
+//	}
+//	
+//	function fnTab(){
+//		oImg.src = arrUrl[num];
+//		for (var i=0; i<aLi.length; i++){
+//			aLi[i].className = '';
+//		}
+//		aLi[num].className = 'hover';
+//	}
+//	fnTab();
+//	
+//	
+//	function Tab(){
+//		for (var i=0; i<aLi.length; i++){
+//			aLi[i].className = '';
+//		}
+//		aLi[num].className = 'hover';
+//	}
+//	
+//	for ( var i=0; i<aLi.length; i++){
+//		aLi[i].index = i;
+//		aLi[i].onmouseover = function (){
+//			num = this.index;
+//			fnTab();
+//		};
+//		
+//	};
+//	oNext.onclick = function(){
+//		num++;
+//		if( num==arrUrl.length){
+//			num=0;
+//		} 
+//		fnTab();
+//	}
+//	oPrev.onclick = function(){
+//		num--;
+//		if( num==-1){
+//			num=arrUrl.length-1;
+//		} 
+//		fnTab();
+//	};
+//	oDiv.onmouseover = function(){
+//		oPrev.style.display = 'block';
+//		oNext.style.display = 'block';
+//	};
+//	
+//	oDiv.onmouseout = function(){
+//		oPrev.style.display = 'none';
+//		oNext.style.display = 'none';
+//	};
+//	
+//	oPrev.onmouseover = function(){
+//		this.style.backgroundPositionY = '-154px';
+//	};
+//	oPrev.onmouseout = function(){
+//		this.style.backgroundPositionY = '-68px';
+//	};
+//	oNext.onmouseover = oPrev.onmouseover;
+//	oNext.onmouseout = oPrev.onmouseout;
+//	
+//};
+
+
